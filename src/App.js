1ee13768b020b95c3b835e1666f26a9dfb51d9aa
oscar <oscar.vasquez@zentro.com.mx>
2023-04-13 15:52:42 -0600

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import {Component, useState} from 'react';
+import { useEffect, useState} from 'react';
 import './App.css';
 
-class App extends Component{
-  state = { contador:0 }
-  incrementar = () =>{
-    this.setState({ contador: this.setState.contador + 1})
+const useContador = (inicial) =>{
+  const [contador, setContador] = useState(0);
+  const incrementar = () =>{
+    setContador( contador + 1)
   }
 
-  render(){
-    return(
-      <div>
-          Contador : {this.state.contador}
-          <button onClick={this.incrementar}>Incrementar</button>
-      </div>
-    )
-  }
+  return [contador,incrementar]
 }
 
-// function App() {
-//   const [contador, setContador] = useState(0);
-//   return (
-//     <div>
-//         Contador : {contador}
-//         <button onClick={() => { setContador( contador + 1)}}>Incrementar</button>
-//     </div>
-//   );
-// }
+function App() {
+  
+  const [contador, incrementar] = useContador(0)
+
+  useEffect(() =>{
+    document.title = contador;
+  }, [contador])
+
+  return (
+    <div>
+        Contador : {contador}
+        <button onClick={incrementar}>Incrementar</button>
+    </div>
+  );
+}
 
 export default App;
