168bef0770fd7b3dc4944c0ab3617523f128ad4c
oscar <oscar.vasquez@zentro.com.mx>
2023-04-14 18:23:58 -0600

    Uncontrolled components, la forma de react (con ref)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 
 const App = () => {
 
-  const submit = (e) =>{
-    e.preventDefault()
-    //console.log(Array.from(new FormData(e.target)))
-    const data = Array.from(new FormData(e.target))
-    console.log(Object.fromEntries(data))
-  }
-
   return(
-    <form onSubmit={submit}>
+    <div>
       <div>
-        <span>
-          lala
-        </span>
-        <input name="campo"/>
+        <span>lala</span>
+        <input type="text" name='campo'/>
       </div>
-      <input name="campo-2"/>
-      <input type="submit" value='Enviar'/>
-    </form>
+      <input type='submit' value='Enviar' onClick={submit}/>
+    </div>
   )
 }
 
