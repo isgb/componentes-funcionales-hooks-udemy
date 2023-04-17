166934383ff53f420cc8c7800bc80c4f966a7fb2
oscar <oscar.vasquez@zentro.com.mx>
2023-04-17 10:42:41 -0600

    Sincronizacion multiples valores

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import {useState} from 'react'
 
 const App = () =>{
 
-  const [value, setValue] = useState('')
-
+  const [value, setValue] = useState({normal: '', texto:''})
   const handleChange = (e) =>{
-    setValue(e.target.value)
+    //setValue({ [e.target.name] : e.target.value })
+    setValue( (state) =>({ 
+      // ...value,
+      // [e.target.name] : e.target.value 
+      ...state,
+      [e.target.name] : e.target.value 
+    }))
   }
   console.log(value)
   return(
      <div>
       {value.length < 5 ? <span>longitud minima de 5</span> : null}
-      <input type="text" name="normal" value={value} onChange={handleChange}/>
+      <input type="text" name="normal" value={value.normal} onChange={handleChange}/>
+      <textarea type='text' name="texto" onChange={handleChange} value={value.texto}/>
      </div>
   )
 
