4a1f589993926256711ee742713fc0c0c9a16384
oscar <oscar.vasquez@zentro.com.mx>
2023-04-17 11:25:14 -0600

    Componente Checkbox

--- src/App.js
+++ src/App.js
@@ -2,31 +2,39 @@ import { useState } from 'react'
 
 const App = () => {
 
-  const [value, setValue] = useState({ normal: 'por defecto', texto: '' ,select : 'chanchofeliz'})
-  const handleChange = (e) => {
+  const [value, setValue] = useState({ normal: 'por defecto', 
+  texto: '' ,select : 'chanchofeliz', check: false})
+  const handleChange = (/*e*/ {target}) => {
     //setValue({ [e.target.name] : e.target.value })
     setValue((state) => ({
       // ...value,
       // [e.target.name] : e.target.value 
       ...state,
-      [e.target.name]: e.target.value
+      [target.name]: target.type === "checkbox" ? target.checked :target.value
     }))
   }
   console.log(value)
   return (
     <div>
       {value.length < 5 ? <span>longitud minima de 5</span> : null}
-      <input type="text" name="normal" value={value.normal} onChange={() => handleChange} />
+      <input type="text" name="normal" value={value.normal} onChange={handleChange} />
       <textarea type='text' name="texto" onChange={handleChange} value={value.texto} />
 
 
-      <select value={value.select} name="select" onChange={ () => handleChange}>
+      <select value={value.select} name="select" onChange={handleChange}>
         <option value=''>-- Seleccione --</option>
         <option value='chanchofeliz'>Chanco Feliz</option>
         <option value='chanchitofeliz'> Chanchito Feliz</option>
         <option value='chanchitotriste'>Chanchito trsite</option>
         <option value='felipe'>Felipe</option>
       </select>
+
+      <input
+        type='checkbox'
+        name='check'
+        onChange={handleChange}
+        checked={value.check}
+      />
     </div>
   )
 
