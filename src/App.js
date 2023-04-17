28f7aa709fe3f223ab8d5a5960464aa7f3477482
oscar <oscar.vasquez@zentro.com.mx>
2023-04-17 11:01:54 -0600

    Etiqueta de Select

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import {useState} from 'react'
+import { useState } from 'react'
 
-const App = () =>{
+const App = () => {
 
-  const [value, setValue] = useState({normal: '', texto:''})
-  const handleChange = (e) =>{
+  const [value, setValue] = useState({ normal: 'por defecto', texto: '' ,select : 'chanchofeliz'})
+  const handleChange = (e) => {
     //setValue({ [e.target.name] : e.target.value })
-    setValue( (state) =>({ 
+    setValue((state) => ({
       // ...value,
       // [e.target.name] : e.target.value 
       ...state,
-      [e.target.name] : e.target.value 
+      [e.target.name]: e.target.value
     }))
   }
   console.log(value)
-  return(
-     <div>
+  return (
+    <div>
       {value.length < 5 ? <span>longitud minima de 5</span> : null}
-      <input type="text" name="normal" value={value.normal} onChange={handleChange}/>
-      <textarea type='text' name="texto" onChange={handleChange} value={value.texto}/>
-     </div>
+      <input type="text" name="normal" value={value.normal} onChange={() => handleChange} />
+      <textarea type='text' name="texto" onChange={handleChange} value={value.texto} />
+
+
+      <select value={value.select} name="select" onChange={ () => handleChange}>
+        <option value=''>-- Seleccione --</option>
+        <option value='chanchofeliz'>Chanco Feliz</option>
+        <option value='chanchitofeliz'> Chanchito Feliz</option>
+        <option value='chanchitotriste'>Chanchito trsite</option>
+        <option value='felipe'>Felipe</option>
+      </select>
+    </div>
   )
 
 }
