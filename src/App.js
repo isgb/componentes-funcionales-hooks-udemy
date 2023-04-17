e791ab7789ac22b8a539d66747af482793f304ce
oscar <oscar.vasquez@zentro.com.mx>
2023-04-17 09:29:51 -0600

    66. Uncontrolled components, la forma de react (con ref)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import { useRef } from 'react';
 
 const App = () => {
+
+  const input = useRef()
+  const file = useRef()
+  const submit = () => {
+    console.log(input.current.value)
+    console.log(file.current.files[0])
+
+    form.append('archivo', file.current.files[0])
+    form.append('campo', input.current.value)
+
+    fetch('/lala', { method: 'POST', body : form})
+  }
 
   return(
     <div>
       <div>
         <span>lala</span>
-        <input type="text" name='campo'/>
+        <input type="text" name='campo' ref={input}/>
+        <input type="file" ref={file}/>
       </div>
       <input type='submit' value='Enviar' onClick={submit}/>
+      
     </div>
   )
 }
